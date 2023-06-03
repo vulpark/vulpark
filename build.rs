3705c789716ab3b2b0c560bd03c6b91251ed30b5
Octal <oliver@olivermakesco.de>
2023-06-03 00:09:53 -0500

    license header

---
+++ build.rs
@@ -0,0 +1,39 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use std::{fs, path::Path};
+
+fn main() {
+    let mut header = include_str!("./license_header").split("\n").map(|it| format!("// {it}\n") ).reduce(|acc, line| format!("{acc}{line}") ).unwrap();
+    header.push('\n');
+    let _ = visit_files(Path::new("src"), &header);
+}
+
+fn visit_files(path: &Path, header: &str) -> Result<(), std::io::Error> {
+    if path.is_file() {
+        return write_header(path, header)
+    }
+    println!("{:?}", path);
+    for new in path.read_dir().unwrap() {
+        visit_files(new?.path().as_path(), header).unwrap()
+    }
+    Ok(())
+}
+
+fn write_header(path: &Path, header: &str) -> Result<(), std::io::Error> {
+    let Some(ext) = path.extension() else {
+        return Ok(())
+    };
+    if ext != "rs" {
+        return Ok(())
+    }
+    let mut text = fs::read_to_string(path)?;
+    if !text.starts_with(header) {
+        let mut tmp = header.to_string();
+        tmp.push_str(&text);
+        text = tmp;
+    }
+    let _ = fs::write(path, text)?;
+    Ok(())
+}
--- src/database/macros.rs
+++ src/database/macros.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 pub(super) macro basic_fetch($col: expr, $id: expr) {
     if let Some(val) = $col.find_one($id, None).await? {
         Some(val.into())
